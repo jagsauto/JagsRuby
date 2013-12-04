8c64355830917540494bebd974d44bb3beb4bfa2
jags <jagadeesh_is@yahoo.com>
2013-12-04 18:24:04 +0000

    removed unwanted cucumber folder

--- cucumber-master/features/lib/step_definitions/json_steps.rb
+++
@@ -1,7 +0,0 @@
-Then /^it should (pass|fail) with JSON:$/ do |pass_fail, json|
-  actual = normalise_json(MultiJson.load(all_stdout))
-  expected = MultiJson.load(json)
-
-  actual.should == expected
-  assert_success(pass_fail == 'pass')
-end
--- cucumber-master/features/lib/step_definitions/language_steps.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-Then(/^cucumber lists all the supported languages$/) do
-  sample_languages = ["Arabic", "български", "Pirate", "English", "日本語"]
-  sample_languages.each do |language|
-    all_output.force_encoding('utf-8').should include(language)
-  end
-end
